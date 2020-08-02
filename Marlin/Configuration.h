77c17df7e88ae1c705c87e04d3a08921c113cdcc
unknown <bergval@gmail.com>
2020-08-02 10:46:33 +0200

    change neopixel pin

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2387,7 +2387,7 @@
 #define NEOPIXEL_LED 
 #if ENABLED(NEOPIXEL_LED)
   #define NEOPIXEL_TYPE   NEO_GRB // NEO_GRBW / NEO_GRB - four/three channel driver type (defined in Adafruit_NeoPixel.h)
-  #define NEOPIXEL_PIN    P1_22 //P2_06      // LED driving pin
+  #define NEOPIXEL_PIN    P1_31//P1_22 //P2_06      // LED driving pin
   //#define NEOPIXEL2_TYPE NEOPIXEL_TYPE
   //#define NEOPIXEL2_PIN    5
   #define NEOPIXEL_PIXELS  1      // Number of LEDs in the strip, larger of 2 strips if 2 neopixel strips are used
