1892e6472e895e56288360fdf3b7d9ca1216ee20
rudelicious <bergval@gmail.com>
2020-12-26 16:14:36 +0100

    config update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1090,7 +1090,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
 #define INVERT_Y_DIR false
-#define INVERT_Z_DIR false
+#define INVERT_Z_DIR true//false
 
 // @section extruder
 
